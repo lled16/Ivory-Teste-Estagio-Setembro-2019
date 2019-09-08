35f7891cf79dea2fac2aadbf41b8dd18ead7423b
unknown <vitoralves1604@gmail.com>
2019-09-08 10:21:29 -0300

    Adicao de projeto 1

---
+++ teste-estagio-ivory-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace teste_estagio_ivory_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            int resultado;
+
+            resultado = Calcular(numero);
+
+            Console.WriteLine("Resultado: {0}", resultado);
+            Console.ReadKey();
+
+               
+        }
+
+        static int Calcular(int numero)
+        {
+            int resultado;
+
+            if(numero <= 0)
+            {
+                return (0);
+            }
+            else
+            {
+                resultado = numero * Calcular(numero - 1);
+                return (resultado);
+            }
+        }
+    }
+}
